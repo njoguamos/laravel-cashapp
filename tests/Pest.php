1d8eef247dd1f7fcb8345f59175358c572230945
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-01-28 02:29:52 +0300

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,14 +4,11 @@ use NjoguAmos\CashApp\Tests\TestCase;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
 use Illuminate\Support\Facades\File;
-use Saloon\MockConfig;
 use Saloon\Config;
 
-MockConfig::setFixturePath('fixtures');
 Config::preventStrayRequests();
 
 uses(TestCase::class)
-    ->beforeEach(fn () => MockClient::destroyGlobal())
     ->in(__DIR__);
 
 function mockRequest(string $request, string $jsonFile, int $status = 200, array $headers = []): MockClient
