6db18c73b4501f4872f97f4657b43bc7a2fbc7c6
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-01-28 02:36:05 +0300

    Update TestCase

--- tests/Feature/CashAppTest.php
+++ tests/Feature/CashAppTest.php
@@ -4,7 +4,7 @@ use NjoguAmos\CashApp\CashApp;
 use NjoguAmos\CashApp\Requests\Network\Brands\ListBrandsRequest;
 
 it(description: 'can successfully get brands from cash app API', closure: function () {
-    $mockClient = mockRequest(
+    $mockClient = $this->mockRequest(
         request: ListBrandsRequest::class,
         jsonFile: 'network/list-brands.json'
     );
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,11 @@
 namespace NjoguAmos\CashApp\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
 use Orchestra\Testbench\TestCase as Orchestra;
 use NjoguAmos\CashApp\CashAppServiceProvider;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
 use Saloon\MockConfig;
 
 class TestCase extends Orchestra
@@ -35,5 +38,16 @@ class TestCase extends Orchestra
         $migration = include __DIR__.'/../database/migrations/create_laravel-cashapp_table.php.stub';
         $migration->up();
         */
+    }
+
+    public function mockRequest(string $request, string $jsonFile, int $status = 200, array $headers = []): MockClient
+    {
+        return MockClient::global([
+            $request => MockResponse::make(
+                body: File::json('tests/fixtures/'.$jsonFile),
+                status: $status,
+                headers: $headers
+            ),
+        ]);
     }
 }
